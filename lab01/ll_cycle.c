e2cefe2aa32eace08e8b37b26a27a0090302853e
Jason-GreyQian <jason20011018@gmail.com>
2025-02-17 10:20:03 +0800

    finish lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,26 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
+/**
+ * if has cycle return 1 else return 0.
+ */
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL) 
+    {
+        return 0;
+    }
+    node *tortoise = head, *hare = head;
+    while (hare != NULL && hare->next != NULL)
+    {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if (hare == tortoise)
+        {
+            return 1; // has cycle
+        }
+        
+    }
+    
     return 0;
 }
--- lab01/ll_cycle.h
+++ lab01/ll_cycle.h
@@ -7,3 +7,9 @@ typedef struct node {
 
 int ll_has_cycle(node *);
 #endif
+
+/**
+ * ifdef: 如果宏已经被定义过，那么后面的代码就不会被编译会被忽略
+ * ifndef: 如果宏没有被定义，那么后面的代码块就会被编译
+ * 本文件这样写的好处在于避免头文件被多次重复包含
+ */
